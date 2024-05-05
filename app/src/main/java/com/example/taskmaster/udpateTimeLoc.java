25455c84b8ebf82fb2afac91a1fad3d93153e8d2
Jumanahali <443200866@student.ksu.edu.sa>
2024-05-05 17:25:11 +0300

    cc

--- app/src/main/java/com/example/taskmaster/udpateTimeLoc.java
+++ app/src/main/java/com/example/taskmaster/udpateTimeLoc.java
@@ -16,7 +16,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-//h
+//ch
 public class udpateTimeLoc extends AppCompatActivity {
     EditText TimeEd,LocEd,rateEd;
             Button statusEd;
